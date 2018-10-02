6a3c3b640f1743dc8d34bfd5e54cce5f671ee801
zegazelda <43519909+zegazelda@users.noreply.github.com>
2018-10-02 16:55:57 +0700

    Update komentar.php

--- komentar.php
+++ komentar.php
@@ -1,3 +1,18 @@
-<?php
+<?php  
+	$error_komentar="";
+	if (isset($_POST['submit'])) {
+		$komentar=$_POST['komentar'];
 
+		if(str_word_count($komentar)<5){
+        $error_komentar= "Komentar harus lebih dari 5 kata!";
+      	}else{
+      		$jumlah=str_word_count($komentar);
+      	echo "Jumlah kata pada Komentar adalah $jumlah .";
+      	}	
+	}
 ?>
+
+<form method="POST">
+	<p>Komentar : <textarea name="komentar"></textarea><span style="color:red"><?php echo $error_komentar; ?></span></p>
+	<input type="submit" name="submit">
+</form>
