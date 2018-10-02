c06bd7f014f7848bec78228ef503178d25fb8466
zegazelda <43519909+zegazelda@users.noreply.github.com>
2018-10-02 16:41:05 +0700

    Update prosesregris.php

--- prosesregris.php
+++ prosesregris.php
@@ -1,4 +1,32 @@
 <?php
+if (isset($_POST['submit'])) {
+	$email = $_POST['email'];
+	$nim = $_POST['nim'];
+	$nama = $_POST['nama']; 
+}
+
+
+include("koneksi.php");
+
+ if(isset($_POST['submit'])){
+
+    $nama = $_POST['nama'];
+    $alamat = $_POST['alamat'];
+    $nim = $_POST['nim'];
+
+    $sql = "INSERT INTO coba (nama, alamat, nim) VALUE ('$nama', '$alamat', '$nim')";
+    $query = mysqli_query($db, $sql);
+
+    if( $query ) {
+        header('Location: ');
+    } else {
+        header('Location: ');
+    }
+
+
+} else {
+    die("Akses dilarang...");
+}
 
 
 ?>
